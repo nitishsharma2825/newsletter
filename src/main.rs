5665ba6bc38d0e47ceea294ce768f9a959cc5308
nitishsharma2825 <sharnit2825@gmail.com>
2025-09-12 20:43:53 +0530

    added configuration management support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::net::TcpListener;
-
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
