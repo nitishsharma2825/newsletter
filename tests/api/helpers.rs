21330424f7f3ca655b3bc96c6ff7395fadf57170
nitishsharma2825 <sharnit2825@gmail.com>
2025-10-10 21:35:06 +0530

    TDD for subscription feature 1: add code for sending email with link

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -4,6 +4,7 @@ use newsletter::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 
 // Ensure that the `tracing` stack is only initialized once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -21,6 +22,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -39,11 +41,15 @@ pub async fn spawn_app() -> TestApp {
     // setup tracing
     Lazy::force(&TRACING);
 
+    // Launch a mock server to stand in for Postmark's API
+    let email_server = MockServer::start().await;
+
     // randomise configuration to ensure test isolation
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string(); // Use a different database for each test case
         c.application.port = 0; // Use a random OS port
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -61,6 +67,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
