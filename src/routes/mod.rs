8894011786a7d64964303b1c4adfe838a1f6a74f
nitishsharma2825 <sharnit2825@gmail.com>
2025-10-10 21:48:38 +0530

    TDD 2: Added template for new confirm token API

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,9 @@
 // declare submodules
 mod health_check;
 mod subscriptions;
+mod subscriptions_confirm;
 
 // re-export public items from submodules to make them accessible from outside
 pub use health_check::*;
 pub use subscriptions::*;
+pub use subscriptions_confirm::*;
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,6 +1,7 @@
 mod health_check;
 mod helpers;
 mod subscriptions;
+mod subscriptions_confirm;
 
 // structuring test as single test executable with scoped submodules for each test.
 // Each submodule can be broken down further when it grows like tests/api/subscriptions/*.rs
