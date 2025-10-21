4f4218e919e3256fdf059a4c43730c38ac216eaa
nitishsharma2825 <sharnit2825@gmail.com>
2025-10-21 14:39:52 +0530

    Timing attack on username prevention code

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,5 +1,8 @@
+use argon2::Algorithm;
 use argon2::Argon2;
+use argon2::Params;
 use argon2::PasswordHasher;
+use argon2::Version;
 use argon2::password_hash::SaltString;
 use newsletter::configuration::{DatabaseSettings, get_configuration};
 use newsletter::startup::{Application, get_connection_pool};
@@ -95,10 +98,14 @@ impl TestUser {
 
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
 
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3)",
