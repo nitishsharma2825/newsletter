86b7e597e7a4dd7101440822b0b5ab96c42e2718
nitishsharma2825 <sharnit2825@gmail.com>
2025-11-01 19:22:18 +0530

    added hmac tag in query params for login failure redirects

--- src/startup.rs
+++ src/startup.rs
@@ -4,7 +4,9 @@ use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletters, subscribe,
 };
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{App, HttpServer, web};
+use secrecy::Secret;
 use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
@@ -14,6 +16,9 @@ pub struct Application {
     port: u16,
     server: Server,
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
 
 impl Application {
     pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
@@ -46,6 +51,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -75,6 +81,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     // wrap the connection in a smart pointer Arc<T>
     let db_pool = web::Data::new(db_pool);
@@ -95,6 +102,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
