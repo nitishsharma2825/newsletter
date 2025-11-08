568eb3220bb2a6e4bbc820bbbae2c37f95c86662
nitishsharma2825 <sharnit2825@gmail.com>
2025-11-08 10:57:06 +0530

    Async newsletter delivery using PG queue table and background workers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use newsletter::configuration::get_configuration;
+use newsletter::issue_delivery_worker::run_worker_until_stopped;
 use newsletter::startup::Application;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
 
@@ -12,7 +13,18 @@ async fn main() -> anyhow::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     // build the application and run the server
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = Application::build(configuration.clone())
+        .await?
+        .run_until_stopped();
+    let worker = run_worker_until_stopped(configuration);
+
+    // All selected futures are polled on same task, concurrency not parallel.
+    // Both run on the same thread, if one branch blocks the thread, all other expressions will be unable to continue
+    // If want parallelism, run on separate threads
+    tokio::select! {
+        _ = application => {},
+        _ = worker => {},
+    };
+
     Ok(())
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,7 +1,11 @@
 use std::time::Duration;
 
+use fake::{
+    Fake,
+    faker::{internet::en::SafeEmail, name::en::Name},
+};
 use wiremock::{
-    Mock, ResponseTemplate,
+    Mock, MockBuilder, ResponseTemplate,
     matchers::{any, method, path},
 };
 
@@ -9,7 +13,13 @@ use crate::helpers::{ConfirmationLinks, TestApp, assert_is_redirect_to, spawn_ap
 
 // helper functions to drive application state for tests
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let name: String = Name().fake();
+    let email: String = SafeEmail().fake();
+    let body = serde_urlencoded::to_string(&serde_json::json!({
+        "name": name,
+        "email": email
+    }))
+    .unwrap();
 
     let _mock_guard = Mock::given(path("/email"))
         .and(method("POST"))
@@ -42,6 +52,10 @@ async fn create_confirmed_susbcriber(app: &TestApp) {
         .unwrap()
         .error_for_status()
         .unwrap();
+}
+
+fn when_sending_an_email() -> MockBuilder {
+    Mock::given(path("/email")).and(method("POST"))
 }
 
 #[tokio::test]
@@ -181,4 +195,48 @@ async fn concurrent_form_submission_is_handled_gracefully() {
         response1.text().await.unwrap(),
         response2.text().await.unwrap()
     );
+}
+
+#[tokio::test]
+async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
+    let app = spawn_app().await;
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string(),
+    });
+
+    // 2 confirmed subscribers
+    create_confirmed_susbcriber(&app).await;
+    create_confirmed_susbcriber(&app).await;
+    app.test_user.login(&app).await;
+
+    // Email delivery fails for 2nd subscriber, thus transaction will rollback
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(200))
+        .up_to_n_times(1)
+        .expect(1) // No request is fired at postmark
+        .mount(&app.email_server)
+        .await;
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(500))
+        .up_to_n_times(1)
+        .expect(1) // No request is fired at postmark
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 500);
+
+    // Email delivery will succeed for both subscribers now, 1st subscriber sees the email again
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1) // No request is fired at postmark
+        .named("Delivery retry")
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 303);
 }
